613c41e3f86a932bc61125556e2059cd8f784a24
Timothy Lebo <timrdf@gmail.com>
2012-11-08 14:36:57 -0500

    documents 2source.sh

---
+++ instances/documents/2source.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+CLASSPATH=$CLASSPATH`../../bin/pl-situate-classpath.sh`
+
+see='https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set'
+CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see $see"}
+CLASSPATH=$CLASSPATH`$CSV2RDF4LOD_HOME/bin/util/cr-situate-classpaths.sh`
+
+sparql="java -Xmx2048m edu.rpi.tw.data.rdf.utils.pipes.stops.SPARQL"
+sparql="java           edu.rpi.tw.data.rdf.utils.pipes.stops.SPARQL"
+
+dryrun="true"
+if [[ "$1" == "--write" || "$1" == "-w" ]]; then
+   dryrun="false"
+fi
+
+let total=`find -f automatic manual -name "*.ttl" | wc -l | awk '{printf("%s",$1)}'`
+echo $total
+
+let "d=0"
+for listing in `find -f automatic manual -name "*.ttl" | $CSV2RDF4LOD_HOME/bin/util/randomize-line-order.py`; do
+   let "d=d+1"
+
+   documents=`rapper -q -g -o ntriples $listing | $sparql ../../bin/rq/documents.rq 2> /dev/null`
+
+   #echo $listing describes $documents
+
+   for document in $documents; do
+      if [[ $document =~ http* ]]; then # Make sure http:// URI
+         document=${document%#*}                             # Strip off the fragment identifier.
+         local=`echo $document | perl -pi -e 's|http?://||'` # Strip off the protocol
+         pushd source &> /dev/null
+            if [ ! -e $local ]; then 
+               if [ "$dryrun" != "true" ]; then
+                  echo "$d/$total $document"
+                  wget --mirror -e robots=off --no-parent $document
+                  echo $document > $local.sd_name &> /dev/null
+               else
+                  echo "$d/$total $document" 
+                  space="`echo $d/$total | sed 's/./ /g'` "
+                  echo "$space^-- wget --mirror -e robots=off --no-parent"
+                  echo "$space $local"
+                  echo
+               fi
+            else
+               echo "$d/$total -- skipping -- $document"
+            fi
+         popd &> /dev/null
+      else
+         echo "WARNING: not an HTTP URI: $document"
+      fi
+   done
+done
+
+if [ "$dryrun" == "true" ]; then
+   echo
+   echo "NOTE: this was a dryrun only, to write files, use --write flag"
+   echo
+fi
--- instances/documents/readme.md
+++ instances/documents/readme.md
@@ -1,6 +1,4 @@
-The [manual/](https://github.com/timrdf/plunk/tree/master/instances/documents/manual) and `automatic/` directories contain Turtle file describing PML files on the web. 
+The [manual/](https://github.com/timrdf/plunk/tree/master/instances/documents/manual) and `automatic/` directories contain Turtle file describing PML files on the web. While `manual/` is maintained manually, `automatic/` is populated by:
 
-While `manual/` is maintained manually, `automatic/` is populated by:
-
-* [../web-directories](https://github.com/timrdf/plunk/tree/master/instances/web-directories)/2source.sh and
-* [../web-directories](https://github.com/timrdf/plunk/tree/master/instances/web-directories)/2source.sh
+* This is populated by [../web-directories](https://github.com/timrdf/plunk/tree/master/instances/web-directories)/2source.sh
+* and [../web-directories](https://github.com/timrdf/plunk/tree/master/instances/web-directories)/2source.sh
